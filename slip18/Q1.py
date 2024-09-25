f4eeff88fced609e008f610d301deabf6e4e3809
PatilKartik1 <patilkartik94@gmail.com>
2024-09-25 19:39:41 +0530

    deleted

--- slip18/Q1.py
+++
@@ -1,9 +0,0 @@
-'''Write an anonymous function to calculate area of square.'''
-
-
-# Define an anonymous function to calculate the area of a square
-area_of_square = lambda side: side ** 2
-
-# Test the anonymous function
-side_length = 5
-print("Area of square with side length", side_length, "is:", area_of_square(side_length))
